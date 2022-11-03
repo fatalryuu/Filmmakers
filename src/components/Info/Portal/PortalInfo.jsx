7501fb4d4205ba5bac527be30cbf87ae3a1319c6
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2022-11-03 11:25:04 +0300

    add portal info

--- src/components/Info/Portal/PortalInfo.jsx
+++ src/components/Info/Portal/PortalInfo.jsx
@@ -4,15 +4,7 @@ import s from "./PortalInfo.module.css"
 const PortalInfo = () => {
     return (
         <div className={s.wrapper}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Ad architecto blanditiis cumque dolore dolores ducimus eligendi eos, error,
-            excepturi facere fugiat fugit id impedit in ipsam iusto laborum magni nihil officiis
-            porro provident quidem quis reiciendis repellendus sapiente sequi sint sit ullam, vel
-            vitae. A ab accusamus adipisci alias aliquid beatae blanditiis consequuntur distinctio
-            eaque ex expedita hic illum ipsa laboriosam laborum magnam, nihil possimus quam quas quia
-            quibusdam quo quod, quos repudiandae rerum sed suscipit temporibus ut velit vitae. Accusamus
-            ad assumenda cupiditate debitis dolor eaque est, inventore ipsam necessitatibus nesciunt officia
-            quia quis rem saepe sed, sit totam.
+            Данный портал содержит информацию о кинорежиссерах Беларуси. На основной странице размещена информация о деятеле дня и описание команды разработчиков портала. Также портал содержит персональную страницу каждого кинорежиссера. На каждой странице идет описание деятеля: биография, фотогалерея с его изображением и изображениями его работ, видео на YouTube об деятеле, место его основной деятельности на карте.
         </div>
     );
 };
